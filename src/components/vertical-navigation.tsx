98fbc820eb2324344eadb8694f7d13a6c93ccb8e
Andre Ruan <andreruan80@gmail.com>
2025-03-10 00:38:21 -0300

    Feat: Melhorias menu de navegação vertical

--- src/components/vertical-navigation.tsx
+++ src/components/vertical-navigation.tsx
@@ -1,19 +1,22 @@
 /* eslint-disable @typescript-eslint/no-empty-object-type */
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import Link from "next/link";
 import React from "react";
 
 const VerticalNavigation = (props: React.PropsWithChildren<{}>) => {
   return (
-    <nav className="fixed left-25 top-30 bg-black/10 border border-white/10 rounded-[80px] backdrop-blur-lg shadow-lg shadow-black/20">
+    <nav className=" bg-black/10 border border-white/10 rounded-[80px] backdrop-blur-lg shadow-lg shadow-black/20">
       <ul className="flex flex-col gap-15 p-5">
         {React.Children.map(props.children, (item, index) => {
           const href = (item as any).props.href || "#";
+          const dataTitle = (item as any).props.dataTitle || "";
           return (
             <li
-              className="hover:scale-125 hover:opacity-100 transition-transform opacity-45"
+              data-title={dataTitle}
+              className="hover:scale-125 hover:opacity-100 transition-transform opacity-45 personalized-title"
               key={index}
             >
-              <a href={href}>{item}</a>
+              <Link href={href}> {item}</Link>
             </li>
           );
         })}
