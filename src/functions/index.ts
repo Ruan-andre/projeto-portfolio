725f858342fe0c021cdf60bd5e44123358aa5c1b
Andre Ruan <andreruan80@gmail.com>
2025-03-09 22:52:26 -0300

    refactor: alteração do arquivo individual para um arquivo onde conterá todas as funções

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -1,3 +1,7 @@
+"use client";
+
+import { useState, useEffect } from "react";
+
 const GetDate = (
   initialDate: Date,
   type: "Y" | "M" | "D" | "FULL",
@@ -58,4 +62,28 @@ const GetDate = (
   return response;
 };
 
-export default GetDate;
+const useTypingEffect = (text: string = "", speed: number) => {
+  const [displayedText, setDisplayedText] = useState("");
+
+  useEffect(() => {
+    if (!text.trim()) {
+      setDisplayedText("");
+      return;
+    }
+
+    let index = 0;
+    setDisplayedText("");
+
+    const interval = setInterval(() => {
+      setDisplayedText(text.slice(0, index + 1));
+      index++;
+      if (index >= text.length) clearInterval(interval);
+    }, speed);
+
+    return () => clearInterval(interval);
+  }, [text, speed]);
+
+  return displayedText;
+};
+
+export { GetDate, useTypingEffect };
