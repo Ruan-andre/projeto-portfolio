aa787ca3b5e27b0d58f82ec5dac656dc41730c7b
Andre Ruan <andreruan80@gmail.com>
2025-03-15 01:50:46 -0300

    Refactor: Remove componente do layout para utilizar na página específica

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import VerticalNavigation from "@/components/vertical-navigation";
-import { Icon } from "../assets/icons";
-import ContactCard from "@/components/contactCard";
+import ContactCard from "@/components/ContactCard";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,42 +18,8 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <body className={`${inter.className} relative min-h-screen pt-[3rem]`}>
-        <header className="fixed z-10 left-25 top-30">
-          <VerticalNavigation>
-            <Icon
-              icon="ic:round-home"
-              width="35"
-              height="35"
-              dataTitle="Inicio"
-              href="#presentation"
-            />
-            <Icon
-              href="#projects"
-              icon="fluent:tabs-16-filled"
-              width="35"
-              height="35"
-              dataTitle="Projetos"
-            />
-            <Icon
-              icon="fa6-solid:code"
-              href="#skills"
-              width="35"
-              height="35"
-              dataTitle="Habilidades"
-            />
-            <Icon
-              href="#contacts"
-              icon="uiw:mail"
-              width="35"
-              height="35"
-              dataTitle="Contato"
-            />
-          </VerticalNavigation>
-        </header>
-        <main className="container max-w-[80%] mx-auto grid grid-cols-4 items-center gap-y-[9rem] pt-[2rem] pb-[10rem]">
-          {children}
-        </main>
-        <footer className="absolute left-0 right-0 bg-background">
+        {children}
+        <footer className="absolute left-0 right-0 bottom-0 bg-background">
           <section id="contacts" className="col-span-4 text-center">
             <h3 className="text-[4rem] pb-[4rem] font-semibold">Contatos</h3>
             <div className="flex items-center justify-center gap-[17rem] bg-shadow-brown-red p-[4rem]">
@@ -71,10 +35,9 @@ export default function RootLayout({
                 title="Linkedin"
               />
               <ContactCard
-                href="mailto:andreruan80@gmail.com"
                 iconName="logos:google-gmail"
                 title="E-mail"
-                info="andreruan80@gmail.com"
+                info="ruan.fullstack@gmail.com"
               />
             </div>
           </section>
