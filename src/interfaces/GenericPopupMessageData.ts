af111de5d74be36f31ed047a81a0e8e9e7e19eb5
Andre Ruan <andreruan80@gmail.com>
2025-03-18 21:37:49 -0300

    Feat: Adiciona tipos e interfaces para utilização no projeto

---
+++ src/interfaces/GenericPopupMessageData.ts
@@ -0,0 +1,9 @@
+interface GenericPopupMessageData {
+  btnCloseClassColor?: string;
+  bgClassColor?: string;
+  textClassColor?: string;
+  width?: string;
+  height?: string;
+}
+
+export default GenericPopupMessageData;
---
+++ src/types/Technologies.ts
@@ -0,0 +1,6 @@
+type Technologies = {
+  name: string;
+  iconName: string;
+};
+
+export default Technologies;
