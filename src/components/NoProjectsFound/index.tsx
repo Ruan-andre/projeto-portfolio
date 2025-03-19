10ca9e54ac5f20318e87c13d3af8c0079afb3035
Andre Ruan <andreruan80@gmail.com>
2025-03-18 21:35:14 -0300

    Feat: Adiciona componente para informar que não há projetos no tipo selecionado

---
+++ src/components/NoProjectsFound/index.tsx
@@ -0,0 +1,9 @@
+const NoProjectsFound = () => {
+  return (
+    <div className="container-center-full">
+      <p>Sem projetos, por enquanto...</p>
+    </div>
+  );
+};
+
+export default NoProjectsFound;
