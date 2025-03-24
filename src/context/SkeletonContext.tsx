4e58f9a4bb31cbd55f7ab2fd0488bef761b65a41
Andre Ruan <andreruan80@gmail.com>
2025-03-24 17:44:21 -0300

    fix: :bug: corrige bug do skeleton ao trocar de página ou recarregar a mesma

--- src/context/SkeletonContext.tsx
+++ src/context/SkeletonContext.tsx
@@ -1,23 +1,31 @@
 "use client";
 
-import { createContext, useState, useContext } from "react";
+import { createContext, useState, useContext, useEffect } from "react";
+import { usePathname } from "next/navigation";
 
 interface SkeletonContextType {
   isLoading: boolean;
   setIsLoading: (loading: boolean) => void;
 }
 
-const skeletonContext = createContext<SkeletonContextType | undefined>(undefined);
+const SkeletonContext = createContext<SkeletonContextType | undefined>(undefined);
 
 export const SkeletonProvider = ({ children }: { children: React.ReactNode }) => {
   const [isLoading, setIsLoading] = useState(true);
+  const pathname = usePathname();
+  useEffect(() => {
+    setIsLoading(true);
 
-  return <skeletonContext.Provider value={{ isLoading, setIsLoading }}>{children}</skeletonContext.Provider>;
+    const timeout = setTimeout(() => setIsLoading(false), 300);
+
+    return () => clearTimeout(timeout);
+  }, [pathname]);
+
+  return <SkeletonContext.Provider value={{ isLoading, setIsLoading }}>{children}</SkeletonContext.Provider>;
 };
 
 export const useSkeleton = () => {
-  const context = useContext(skeletonContext);
-
-  if (!context) throw new Error("useSkeleton deve ser usado dentro de FooterProvider");
+  const context = useContext(SkeletonContext);
+  if (!context) throw new Error("useSkeleton deve ser usado dentro de SkeletonProvider");
   return context;
 };
