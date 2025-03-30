ee72d868190b0b9b793034fa930f600ee2b12191
Andre Ruan <andreruan80@gmail.com>
2025-03-30 02:13:15 -0300

    delete: Remove api lastUpdated e arquivos para seu uso

--- src/app/api/lastUpdated/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from "next/server";
-import jsonFile from "./lastUpdated.json";
-export async function GET() {
-  const lastUpdated = jsonFile.lastUpdated;
-
-  return NextResponse.json({ lastUpdated });
-}
