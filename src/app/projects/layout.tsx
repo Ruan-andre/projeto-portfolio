d5cfe664273a99a0438649c4855500353ff27565
Andre Ruan <andreruan80@gmail.com>
2025-03-30 02:07:36 -0300

    Refactor: Refatoração de código

--- src/app/projects/layout.tsx
+++ src/app/projects/layout.tsx
@@ -3,9 +3,9 @@
 import { Icon } from "../../../public/assets/icons";
 import VerticalNavigation from "@/components/VerticalNavigation";
 import { usePathname } from "next/navigation";
-import ProjectsTypes from "@/enums/ProjectsTypes";
 import { ProjectsModalProvider } from "@/context/ProjectsModalContext";
 import ProjectPopupDetails from "@/components/ProjectPopupDetails";
+import { projectsConfig } from "./projectsConfig";
 import "./projects.css";
 
 export default function ProjectsLayout({ children }: { children: React.ReactNode }) {
@@ -18,74 +18,24 @@ export default function ProjectsLayout({ children }: { children: React.ReactNode
 
 function LayoutContent({ children }: { children: React.ReactNode }) {
   const pathName = usePathname();
-  const project = pathName.substring(pathName.lastIndexOf("/") + 1);
-
-  const attrIcon1 = {
-    nameIcon: project.includes(ProjectsTypes.Utilities)
-      ? "mingcute:game-2-fill"
-      : project.includes(ProjectsTypes.Games)
-      ? "ion:person"
-      : project.includes(ProjectsTypes.Personal)
-      ? "mdi:gear"
-      : "",
-    dataTitleIcon: project.includes(ProjectsTypes.Utilities)
-      ? "Projetos de Jogos"
-      : project.includes(ProjectsTypes.Games)
-      ? "Projetos Pessoais"
-      : project.includes(ProjectsTypes.Personal)
-      ? "Projetos Utilitários"
-      : "",
-    hrefIcon: project.includes(ProjectsTypes.Utilities)
-      ? "./games"
-      : project.includes(ProjectsTypes.Games)
-      ? "./personal"
-      : project.includes(ProjectsTypes.Personal)
-      ? "./utilities"
-      : "",
-  };
-
-  const attrIcon2 = {
-    nameIcon: project.includes(ProjectsTypes.Utilities)
-      ? "ion:person"
-      : project.includes(ProjectsTypes.Games)
-      ? "mdi:gear"
-      : project.includes(ProjectsTypes.Personal)
-      ? "mingcute:game-2-fill"
-      : "",
-    dataTitleIcon: project.includes(ProjectsTypes.Utilities)
-      ? "Projetos Pessoais"
-      : project.includes(ProjectsTypes.Games)
-      ? "Projetos Utilitários"
-      : project.includes(ProjectsTypes.Personal)
-      ? "Projetos de Jogos"
-      : "",
-    hrefIcon: project.includes(ProjectsTypes.Utilities)
-      ? "./personal"
-      : project.includes(ProjectsTypes.Games)
-      ? "./utilities"
-      : project.includes(ProjectsTypes.Personal)
-      ? "./games"
-      : "",
-  };
-
+  const projectPath = pathName.split("/").pop();
+  const iconsFiltered = projectsConfig.filter((item) => item.path !== projectPath);
   return (
     <>
       <VerticalNavigation>
         <Icon icon="ic:round-home" width="35" height="35" data-title="Inicio" href="/" />
-        <Icon
-          icon={attrIcon1.nameIcon}
-          width="35"
-          height="35"
-          data-title={attrIcon1.dataTitleIcon}
-          href={attrIcon1.hrefIcon}
-        />
-        <Icon
-          icon={attrIcon2.nameIcon}
-          width="35"
-          height="35"
-          data-title={attrIcon2.dataTitleIcon}
-          href={attrIcon2.hrefIcon}
-        />
+        {iconsFiltered.map((item, index) => {
+          return (
+            <Icon
+              key={index}
+              icon={item.icon}
+              width="35"
+              height="35"
+              data-title={item.title}
+              href={`./${item.path}`}
+            />
+          );
+        })}
       </VerticalNavigation>
       {children}
       <ProjectPopupDetails />
