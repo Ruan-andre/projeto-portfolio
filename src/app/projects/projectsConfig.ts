f128e9f9e0a27bb512ece00a3bcee853944fb246
Andre Ruan <andreruan80@gmail.com>
2025-03-30 02:08:07 -0300

    Feat: Adiciona arquivo de confiuração dos projetos para alternar os ícones do menu de navegação vertical

---
+++ src/app/projects/projectsConfig.ts
@@ -0,0 +1,6 @@
+export const projectsConfig = [
+  { path: "utilities", icon: "mdi:gear", title: "Projetos Utilitários" },
+  { path: "academics", icon: "ion:book", title: "Projetos Acadêmicos" },
+  { path: "personal", icon: "ion:person", title: "Projetos Pessoais" },
+  { path: "games", icon: "mingcute:game-2-fill", title: "Projetos de Jogos" },
+];
