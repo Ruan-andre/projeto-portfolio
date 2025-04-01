aefc7e147b74786b911e2bacadfdc2f1df572986
Andre Ruan <andreruan80@gmail.com>
2025-04-01 12:04:59 -0300

    Refactor: Refatora código para melhor adaptação do skeleton no mobile

--- src/components/skeletons/home/index.tsx
+++ src/components/skeletons/home/index.tsx
@@ -1,23 +1,29 @@
 import Skeleton from "react-loading-skeleton";
 
 const SkeletonHome = () => {
+  const isMobile = /Mobi|Android/i.test(navigator.userAgent);
   return (
     <div className="max-w-[80%] m-auto flex flex-col gap-[15rem]">
-      <div className="flex gap-[22rem]">
-        <div className="flex flex-col justify-center gap-[6rem]">
-          <Skeleton width={570} height={40} />
-          <Skeleton width={692} height={40} />
-          <Skeleton width={692} height={162} />
+      <div className="flex max-sm:flex-col-reverse gap-[22rem]">
+        <div className="flex flex-col max-sm:max-w-[100%] justify-center gap-[6rem]">
+          <Skeleton style={{ width: "570px", height: "40px" }} />
+          <Skeleton style={{ width: isMobile ? "80%" : "692px", height: "40px" }} />
+          <Skeleton style={{ width: isMobile ? "80%" : "692px", height: "162px" }} />
         </div>
-        <Skeleton width={450} height={450} circle />
+        <div className="max-sm:max-w-[100%]">
+          <Skeleton
+            style={{ width: isMobile ? "80%" : "450px", height: isMobile ? "80%" : "450px" }}
+            circle
+          />
+        </div>
       </div>
       <div className="flex flex-col items-center gap-[5rem]">
         <Skeleton width={160} height={45} className="text-center" />
         <div className="flex gap-[2rem]">
-          {Array(3)
+          {Array(isMobile ? 1 : 3)
             .fill(0)
             .map((_, index) => {
-              return <Skeleton key={index} width={498} height={516} borderRadius={45} />;
+              return <Skeleton key={index} height={516} borderRadius={45} />;
             })}
         </div>
       </div>
