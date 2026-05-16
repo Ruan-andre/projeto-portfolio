2ca1fd1c212851e13ae63787fda46b92c8c73cb1
ruan-andre <andre.ruan@agapeconsultoria.com.br>
2026-05-15 22:58:54 -0300

    refactor(eslint): substitui configurações do ESLint para uso do FlatCompat e atualiza dependências

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,18 @@
-import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
-import nextTypescript from "eslint-config-next/typescript";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: import.meta.dirname,
+});
 
-const eslintConfig = [...nextCoreWebVitals, ...nextTypescript, {
-  ignores: ["node_modules/**", ".next/**", "out/**", "build/**", "next-env.d.ts"]
-}];
-
-export default eslintConfig;
+export default [
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    ignores: [
+      "node_modules/**",
+      ".next/**",
+      "out/**",
+      "build/**",
+      "next-env.d.ts",
+    ],
+  },
+];
