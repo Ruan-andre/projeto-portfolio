3e352abe181f7e4676a587ccbc22a78108c5a744
ruan-andre <andre.ruan@agapeconsultoria.com.br>
2026-05-15 22:49:10 -0300

    feat(hero): adiciona currículo em português e inglês, e implementa botão de download no componente Hero

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -5,6 +5,9 @@ import { useLanguage } from "@/context/LanguageContext";
 import useTypingEffect from "@/hooks/useTypingEffect";
 import profilePicture from "../../../public/assets/img/profile-picture.jpg";
 
+import { Icon } from "@iconify/react";
+import Link from "next/link";
+
 interface HeroProps {
   myAge: string | number;
   experienceYears: string | number;
@@ -20,6 +23,47 @@ const Hero = ({ myAge, experienceYears }: HeroProps) => {
 
   return (
     <header id="presentation" className="hero-container">
+      <div className="hero-text-content">
+        <h1 className="hero-title">
+          {t("Olá, eu sou", "Hello, I am")}{" "}
+          <span className="text-special tracking-widest uppercase block md:inline">André Ruan!</span>
+        </h1>
+
+        <div className="hero-subtitle-wrapper">
+          <h2 className="hero-subtitle">
+            {t("Sou", "I am")}{" "}
+            <span className="text-special tracking-widest font-semibold typing-effect">{typingTitle}</span>
+          </h2>
+        </div>
+
+        <p className="hero-description">
+          {t("Tenho ", "I'm ")}
+          {myAge}
+          {t(" anos e atuo na área da tecnologia há ", " years old and have been working in technology for ")}
+          <span className="text-special font-normal opacity-100">
+            {experienceYears} {t("anos.", "years.")}
+          </span>
+          {t(
+            " Minha paixão pela programação começou quando iniciei a faculdade de Análise e Desenvolvimento de Sistemas. Hoje, trabalho profissionalmente como Desenvolvedor Full Stack, mas admito que o backend é minha verdadeira paixão.😂❤️",
+            " My passion for programming started when I began my systems analysis and development degree. Today, I work professionally as a Full Stack Developer, but I admit that backend is my true passion.😂❤️",
+          )}
+        </p>
+
+        <Link
+          href={t(
+            "/assets/curriculo/Curriculo_Andre_Ruan_Desenvolvedor_PT-BR.pdf",
+            "/assets/curriculo/Curriculum_Andre_Ruan_Developer_EN.pdf",
+          )}
+          download
+          className="btn-download-cv group"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Icon icon="ph:file-pdf-bold" className="btn-download-cv-icon group-hover:scale-110" />
+          {t("Download CV", "Download resume")}
+        </Link>
+      </div>
+
       <div className="hero-profile-wrapper group">
         <div className="hero-profile-glow"></div>
         <a
@@ -27,41 +71,9 @@ const Hero = ({ myAge, experienceYears }: HeroProps) => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Image
-            src={profilePicture}
-            alt="André Ruan"
-            width={320}
-            height={320}
-            className="hero-profile-image"
-            priority
-          />
+          <Image src={profilePicture} alt="André Ruan" className="hero-profile-image" priority />
         </a>
       </div>
-
-      <h1 className="hero-title">
-        {t("Olá, eu sou", "Hello, I am")}{" "}
-        <span className="text-special tracking-widest uppercase">André Ruan!</span>
-      </h1>
-
-      <div className="hero-subtitle-wrapper">
-        <h2 className="hero-subtitle">
-          {t("Sou", "I am")}{" "}
-          <span className="text-special tracking-widest font-semibold typing-effect">{typingTitle}</span>
-        </h2>
-      </div>
-
-      <p className="hero-description">
-        {t("Tenho ", "I'm ")}
-        {myAge}
-        {t(" anos e atuo na área da tecnologia há ", " years old and have been working in technology for ")}
-        <span className="text-special font-normal opacity-100">
-          {experienceYears} {t("anos.", "years.")}
-        </span>
-        {t(
-          " Minha paixão pela programação começou quando iniciei a faculdade de Análise e Desenvolvimento de Sistemas. Hoje, trabalho profissionalmente como Desenvolvedor Full Stack, mas admito que o backend é minha verdadeira paixão.😂❤️",
-          " My passion for programming started when I began my systems analysis and development degree. Today, I work professionally as a Full Stack Developer, but I admit that backend is my true passion.😂❤️",
-        )}
-      </p>
     </header>
   );
 };
