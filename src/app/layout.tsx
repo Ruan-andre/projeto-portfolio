64a43b4cb5b85bfa31c17796441b934c412e3147
ruan-andre <andre.ruan@agapeconsultoria.com.br>
2026-05-15 23:19:25 -0300

    refactor(layout): adiciona suppressHydrationWarning ao elemento html

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="pt-BR">
+    <html lang="pt-BR" suppressHydrationWarning>
       <LanguageProvider>
         <GenericPopupMessageProvider>
           <SkeletonTheme baseColor="#202020" highlightColor="#444">
